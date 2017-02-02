f277d98beea43c3a536aee1eee0a6f748e436cee
David Delmar <ddelmar@where.com>
2017-02-02 14:59:10 -0500

    messing around with roster pg

---
+++ src/pages/Bootcamp/Roster/Roster.js
@@ -0,0 +1,34 @@
+import './Roster.scss';
+import React from 'react';
+import BinaryText from '../../../components/BinaryText/BinaryText';
+import RosterMembers from './RosterMembers/TeamMember/TeamMember';
+import RouteTransition from '../../../components/RouteTransition/RouteTransition';
+
+import Hero from '../../../components/Containers/Hero/Hero';
+import RosterHeroImage from './roster-hero.png';
+
+const Team = (props) => {
+  return (
+    <RouteTransition>
+      <Hero
+        titleLineOne="The team"
+        image={RosterHeroImage}
+        centered={true}
+      >
+        <div className="divider divider__red" />
+        <p>
+          Strong student leadership, dedicated staff, and an impressive board make Resilient Coders
+          what it is. The ranges of experience and background vary across the team. But our people
+          have in common a belief in the social mission, and the muscle to make real change.
+        </p>
+      </Hero>
+      <div className="team">
+        <div className="team-container">
+          <RosterMembers/>
+        </div>
+      </div>
+    </RouteTransition>
+  )
+}
+
+export default Team
---
+++ src/pages/Bootcamp/Roster/RosterMembers/TeamMembers.js
@@ -0,0 +1,45 @@
+import './TeamMembers.scss';
+import React from 'react';
+import TeamMember from './TeamMember/TeamMember';
+import teamMembersConfig from './teamMembersConfig';
+import ButtonPrimary from '../../../../components/Buttons/ButtonPrimary';
+import {Link} from 'react-router';
+
+class Work extends React.Component {
+  renderTeamMembers() {
+    return teamMembersConfig.map((item, i) => (
+      <TeamMember
+        key={i}
+        image={item.image}
+        name={item.name}
+        title={item.title}
+        bio={item.bio}
+      />
+    ));
+  }
+  render() {
+    return (
+      <div className="team-members">
+        {this.renderTeamMembers()}
+        <div className="team-members__join">
+          <div className="team-members__join__container">
+            <div>
+              <h2>
+                Interested in joining?
+              </h2>
+              <p>
+                There's more than one way to join the code literacy movement. Check out our
+                opportunities for both companies and individuals.
+              </p>
+              <ButtonPrimary to="/get-involved">
+                Get Involved
+              </ButtonPrimary>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Work;
