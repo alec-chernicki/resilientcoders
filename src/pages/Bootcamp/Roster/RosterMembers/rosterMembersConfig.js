4bd9790f06830ab1381a94f627b9b07524bcf1cc
David Delmar <ddelmar@where.com>
2017-02-10 14:25:51 -0500

    started adding roster content

--- src/pages/Bootcamp/Roster/RosterMembers/rosterMembersConfig.js
+++ src/pages/Bootcamp/Roster/RosterMembers/rosterMembersConfig.js
@@ -11,121 +11,48 @@ import FredImage from './TeamMemberImages/fred.png';
 
 export default [
   {
-    name: 'David Delmar',
+    name: 'Stephen Simpson',
     image: DelImage,
-    title: 'Executive Director, Founder',
-    bio: `David Delmar, Executive Director, is an award-winning designer and interface developer
-    with experience working with startups and established brands. He most recently built and led an
-    exceptional creative group at PayPal’s Boston office, uniting design and development efforts into a
-    single team. Together, they were responsible for designing and building first-to-market digital
-    advertising concepts for big brands. He believes in knowledge-sharing, and so helped launch the
-    PayPal Incubator (fka Start Tank), and served as its User Experience subject matter expert, advising
-    startups on product design. He sits on the Boston University College of Fine Arts Dean’s Advisory Board.`
+    title: '',
+    bio: `I'm immensely proud to be a Howard-bred hustler with
+    an insatiable desire for driving transformative social impact.  My experiences in political organizing,
+    education reform, research, and consulting have given me a strong foundation in outreach, data analysis,
+     and organizational strategy.  Through building teams and facilitating collaboration, I embrace the
+     process of refining products to most efficiently create the greatest value.  My commitment to continuous
+     improvement have brought me to Resilient Coders and Startup Institute.
+     Through these experiences I have acquired the distinguishing marketing insights, business acumen and
+     web development skills to drive innovation and value creation in the private sector.`
   },
   {
-    name: 'Leon Noel',
+    name: 'Tristian Barboza-Graham',
     image: LeonImage,
-    title: 'Managing Director of Engineering',
-    bio: `Leon Noel is an internationally recognized software
-    developer and entrepreneur. Before joining the Resilient Coders team, he co-founded SocialSci,
-    a scientific surveying company relied on by over 4000 academic institutions and held an associate
-    position at Harvard University. Besides his lecturing work at Harvard
-    Business School, MIT Sloan School of Business, and General Assembly,
-    Leon has also worked with major brands such as Shawmut Construction, BMW and Capital One to help train
-    their engineering teams and bring new technology to market. His technological pursuits have been
-    featured in numerous publications including Tech Crunch, Black Enterprise, and the Boston Globe.`
+    title: '',
+    bio: `I am a web developer, designer & artist. I am a strong advocate for balance; in fact, I am ambidextrous.
+    That, plus my fine arts background, afford me a unique perspective in my work. I've learned a new set of skills
+    at Resilient Coders Bootcamp: HTML, CSS, some JavaScript fundamentals, jQuery, and UX Design. I have a strong interest
+    in pursuing a career in design.`
   },
   {
-    name: 'Helen Rosenfeld',
+    name: 'Pariss Chandler',
     image: HelenImage,
-    title: 'Head of Strategic Partnerships',
-    bio: `Helen is committed to working on behalf of youth. She has led a series of youth
-      development organizations, utilizing entrepreneurship as a tool to increase skills of
-      underserved youth for success in school and life. She loves the startup phase of
-      organizational development, raising funds, and building strategic partnerships. A grad of
-      UPenn (BA) and Cambridge (MPhil), she is an Overseer with the Boston Children's Museum and
-      is an advisor to Friends of Seva Mandir, supporting the non-profit that she first worked with
-      in Rajasthan, India where she began her career.`
+    title: '',
+    bio: `Pariss is a creative and diligent individual who uses her love for front end web development
+    to design vibrant and engaging products. Coming from an artistic background of acting, filmmaking,
+    and writing, she loves grabbing the attention of an audience, and rewarding it with a great experience.
+    Pariss enjoys working with diverse and casual teams, with colleagues who believe in constantly honing
+    their skills. She wants to contribute to an environment in which people are encouraged to learn, teach, and thrive.`
   },
 
 
   {
-    name: 'Muigai Unaka',
+    name: 'Maria Ines Peniche',
     image: MuigaiImage,
-    title: 'Resilient Lab Lead',
-    bio: `Muigai is a Senior at Northeastern University, pursuing a dual BFA in graphic design
-      and interactive media. He has a passion for user interface design, and hopes to one day be
-      part of a special projects team, designing and building products that redefine how we interact
-      with each other. Also, his great-grandfather is Jomo Kenyatta.`
+    title: '',
+    bio: `Maria is a hard-working, fast learner who is always ready for the next challenge.
+    She's comfortable working in a fast-paced environment with a diverse workforce in a multicultural
+    setting. She has excellent analytical, written and oral communication skills in both English and Spanish.
+    She's fluently bilingual. Learn more about Maria, and about her experiences as a member of the Dream 9, on
+    NPR: http://latinousa.org/episode/the-dream-9-rb/`
   },
-  {
-    name: 'Alec Ortega',
-    image: AlecImage,
-    title: 'Mentorship Lead',
-    bio: `Alec has been a mentor with Resilient for the last two years. He's passionate about
-    diversity in tech and social innovation. He believes that diversity means not just acknowledging,
-    but embracing the fact that some can get to "Point B" regardless of their "Point A". To
-    this end, he's also an award-winning instructor at at Startup Institute teaching the Web Design
-    track. During the day, he works as a front end engineer at HubSpot and consumes too much
-    coffee for his own good.`
-  },
-  {
-    name: 'Chris Swenor',
-    image: ChrisImage,
-    title: 'Technical Advisor',
-    bio: `Taught himself how to code at the age of 12. Discovered he loved it and couldn't get
-      enough. Decided to bypass college and dive right into his career as a developer. Worked his
-      way up through the developer ranks until it was time inspire other developers to make
-      amazing things. Currently the CEO and Co-Founder of East Coast Product (ECP) -- a design
-      and development agency that works to be the fastest, most efficient way to build digital
-      product and get companies to their next milestone.`
-  },
-  {
-    name: 'Joshua Summers',
-    image: JoshImage,
-    title: 'Board Chair',
-    bio: `Joshua Summers is a serial entrepreneur that believes the best companies are built in
-      and around Boston. As the Co-founder and CEO of clypd Joshua is leading the charge in
-      building the next evolution in tv advertising sales models by bridging the gap between
-      traditional linear TV ads and programmatic solutions. Joshua holds a bachelors degree
-      in Industrial Management with an additional major in Economics from Carnegie Mellon
-      University. Joshua lives with his forgiving wife Rachel and awesome kids Zachary and
-      Alisha in Lexington, MA.`
-  },
-  {
-    name: 'Pamela Aldsworth',
-    image: PamelaImage,
-    title: 'Board',
-    bio: `Pamela Aldsworth is a managing director with Silicon Valley Bank Capital. In this role,
-      she oversees strategic relationships with venture capital firms by leveraging the diverse
-      financial services of SVB Financial Group (SVB) platform. She is primarily focused on managing
-      relationships with a key subset of venture capital firms that have historically and
-      consistently achieved top decile returns. She builds strategic roadmanps with firms in her
-      coverage universe to ensure that SVB is providing the best possible insights, guidance,
-      products and services to the partnership and their portfolio companies.`
-  },
-  {
-    name: 'C.A. Webb',
-    image: CaImage,
-    title: 'Board',
-    bio: `C.A. runs the Community Foundation at Underscore VC, incorporating the Startup Secrets
-    program and works on their broader community outreach from which our _Core groups of
-    experienced entrepreneurs are formed. As a proven community builder and marketer, C.A. comes
-    from her previous post as Executive Director of the New England Venture Capital Association,
-    where she reinvented and reinvigorated the 40-year old association. Recently, C.A. was
-    recognized as an Innovation All Star by Boston Business Journal and is passionate about
-    creating powerful communities that can have a meaningful and lasting impact on business.`
-  },
-  {
-    name: 'Frederick Townes',
-    image: FredImage,
-    title: 'Board',
-    bio: `Fred builds and leads teams that craft web platforms, products, and communities.
-    From software development and architecture, to product marketing strategy, to business leadership and
-    operations, he thrives at the intersection of technology, strategy, and design. He's worked with
-    organizations of all sizes on two continents, from The Coca-Cola Company and Staples to Sony and Microsoft,
-    to develop and drive business models at scale. Now, he's applying his technical experience as both developer
-    and entrepreneur to transforming the real estate industry with Placester, advising early-stage startups,
-    and speaking at digital marketing and technology events around the world.`
-  },
+
 ]
