af95f42c13ab99de5a78fc276a49f70f00afc0b8
Alec Ortega <alecortega@live.com>
2017-02-19 20:55:16 -0500

    Add better tracking for google

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routeConfig/routes';
+import GoogleAnalytics from 'react-ga';
+
+GoogleAnalytics.initialize('UA-66576358-3');
+
+const logPageView = () => {
+  GoogleAnalytics.set({ page: window.location.pathname });
+  GoogleAnalytics.pageview(window.location.pathname);
+};
 
 render((
-  <Router history={browserHistory}>
+  <Router history={browserHistory} onUpdate={logPageView}>
     {routes}
   </Router>
 ), document.getElementById('root'));
