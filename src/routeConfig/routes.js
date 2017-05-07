e17f58aa2c2a6d314f7d49e571272e358da1e9ef
Alec Ortega <alecortega@live.com>
2017-05-07 18:23:11 -0400

    Add redirect back in

--- src/routeConfig/routes.js
+++ src/routeConfig/routes.js
@@ -46,8 +46,8 @@ export default(
       <Route path="student" component={GetInvolvedStudent}/>
       <Redirect from="mentor" to='volunteer' />
     </Route>
-    
-    {/* This is a catch all instead of 404-ing, DO NOT REMOVE */}
-    <Redirect from="*" to='/' />
+
+    {/* This is a catch-all instead of a 404. DO NOT REMOVE */}
+    <Redirect from="*" to="/" />
   </Route>
 );
