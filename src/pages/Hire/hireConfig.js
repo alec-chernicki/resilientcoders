76eedc59121e820e22fe91649c1603b5d7a2deb3
muigaiunaka <muigaiunaka@gmail.com>
2018-10-19 16:21:56 -0400

    created Hire page

---
+++ src/pages/Hire/hireConfig.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'Wayfair',
+    note: 'Diverse and inclusive pipelines',
+    image: 'https://resilientcoders.s3.amazonaws.com/i/lab/logo_wanderu.png',
+    description: "Wayfair Labs, an internal bootcamp program in software engineering, is an innovative response to the tight software engineering labor market Boston is experiencing. Thanks to this program, Wayfair is actively expanding its pool of candidates by making opportunities available for people with non-traditional backgrounds, including Resilient Coders graduates. Through our partnership, Wayfair is being intentional about building a diverse and inclusive pipelines.",
+  },
+  {
+    title: 'Rstudio',
+    note: 'The magic of mentorship',
+    image: 'https://resilientcoders.s3.amazonaws.com/i/lab/logo_cityawake.png',
+    description: "It is through our work with RStudio that we realized the true extent of the value of mentorship. In collaboration with Resilient Coders, RStudio developed an onboarding plan spelling out the expectations for the hire and reserving time for continued learning. This went a long way in making sure that all of us stood behind Karen's continued success on the job. Not surprisingly, at the end of a four-month contract, Karen was hired full-time.",
+  },
+  {
+    title: 'DigitasLBI',
+    note: 'Internal champions',
+    image: 'https://resilientcoders.s3.amazonaws.com/i/lab/logo_helpscout.png',
+    description: "Digitas’ leadership have been early supporters of Resilient Coders as well as  thought partners on how to bring more diversity to the agency world. When they hired Angel, this talented, newly coder stepped into a company where he had internal champions who were familiar with Resilient Coders and welcomed him with arms wide open. This was a simple but important step in building trust and chipping away at the imposter syndrome that can preclude many people -- disproportionately women and people of color -- from launching or advancing their careers in tech."
+  },
+]
