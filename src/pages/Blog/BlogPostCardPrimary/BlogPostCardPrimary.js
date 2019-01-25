f7eba185ed4008779a6e2e4d8c9d5d971c1d2d30
muigaiunaka <muigaiunaka@gmail.com>
2019-01-25 11:45:16 -0500

    primary blog post ui

--- src/pages/Blog/BlogPostCardPrimary/BlogPostCardPrimary.js
+++ src/pages/Blog/BlogPostCardPrimary/BlogPostCardPrimary.js
@@ -1,26 +1,38 @@
 import React, { PureComponent } from 'react';
 import UIButton from 'UILibrary/button/UIButton';
+import {Link} from 'react-router';
 import styles from './BlogPostCardPrimary.scss';
+import MediaQuery from 'react-responsive';
 
 class BlogPostCardPrimary extends PureComponent {
     constructor(props) {
         super(props);
     }
     render() {
-        const { index, post } = this.props;
+        const { post } = this.props;
+        const { 
+            title, 
+            categories, 
+            slug,
+            featured_image
+        } = post;
+        
         return (
-            <div key={post.title} className="blog-post-card primary">
+            <Link key={title} className="blog-post-card primary" to={`/blog/post/${slug}`}>
                 <div className="blog-post-card-content">
-                    <h4>{post.categories[0].name}</h4>
-                    <h1>{post.title}</h1>
-                    <UIButton to="/bootcamp/roster">
+                    <h4>{categories[0].name ? categories[0].name : "Post"}</h4>
+                    <h1>{title}</h1>
+                    <UIButton to={`/blog/post/${slug}`}>
                         Read Post
                     </UIButton>
                 </div>
-                <div className="blog-post-card-image">
-                    <img src={post.featured_image} alt={post.title} />
-                </div>
-            </div>
+                { 
+                    featured_image &&
+                    <div className="blog-post-card-image">
+                        <img src={featured_image} alt={title} />
+                    </div>
+                }
+            </Link>
         )
     }
 
