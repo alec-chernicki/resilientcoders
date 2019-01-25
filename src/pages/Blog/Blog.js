c756965bc10c6f03fa8bf3a36e6bd8019ee629fa
muigaiunaka <muigaiunaka@gmail.com>
2019-01-25 11:41:35 -0500

    ribbon cutting on blog page

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -2,28 +2,57 @@ import styles from './Blog.scss';
 import CSSModules from 'react-css-modules';
 import React, { PureComponent } from 'react';
 import RouteTransition from 'components/RouteTransition/RouteTransition';
+import { Link } from 'react-router';
 import UIButton from 'UILibrary/button/UIButton';
 import UILayout from 'UILibrary/layout/UILayout';
-import BlogPost from './BlogPost/BlogPost';
+import UISection from 'UILibrary/layout/UISection';
+import UIFlexRow from 'UILibrary/grid/UIFlexRow';
+import UIFlex from 'UILibrary/grid/UIFlex';
+import UIContentCardButton from 'UILibrary/button/UIContentCardButton';
+
 import BlogPostCardPrimary from './BlogPostCardPrimary/BlogPostCardPrimary';
 import BlogPostCardSecondary from './BlogPostCardSecondary/BlogPostCardSecondary';
 import BlogPostCard from './BlogPostCard/BlogPostCard';
 
+import Helmet from "react-helmet";
 import Butter from 'buttercms';
 const butter = Butter('3b949f80849c37c52f9a9bb1a7c5d5d393e7895b');
 
+
+const MEDIA_PRESS_TYPES = [
+    {
+      title: 'News',
+      text: 'Keep up with our press, awards and social media',
+      url: '/news',
+      buttonText: "See News"
+    },
+    {
+      title: 'Tutorials',
+      text: 'Step your programming game up step by step with our alum and mentors',
+      url: '/',
+      buttonText: "See Tutorials"
+    },
+    {
+      title: 'Spotlight',
+      text: 'Hear about the software engineers coming out of our program, from the people employing them',
+      url: '/',
+      buttonText: "See Spotlight"
+    }
+];
+  
 class Blog extends PureComponent {
     constructor(props) {
         super(props);
 
         this.state = {
-            content: [],
+            blogPosts: [],
             isLoading: false,
             error: null
         };
     }
+
     componentDidMount() {
-        let data = butter.post.list({ })
+        let data = butter.post.list({})
             .then(response => {
                 if (response.status == 200) {
                     console.log(response.data);
@@ -32,42 +61,117 @@ class Blog extends PureComponent {
                     throw new Error('Something went wrong ...');
                 }
             })
-            .then(data => this.setState({ content: data, isLoading: false }))
-            .then(() => console.log(this.state.content))
+            .then(data => this.setState({ blogPosts: data, isLoading: false }))
+            .then(() => console.log(this.state.blogPosts))
             .catch(error => this.setState({ error, isLoading: false }));
-
     }
+
+    renderCards() {
+        return MEDIA_PRESS_TYPES.map((item, key) => {
+          return (
+            <UIFlex
+              key={key}
+              basis="31%"
+              grow={0}
+              shrink={0}
+              className="index-10 m-sm-bottom-4"
+            >
+              <Link to={item.url}>
+                <UIContentCardButton
+                    use="primary"
+                  title={item.title}
+                  text={item.text}
+                  buttonText={item.buttonText}
+                />
+              </Link>
+            </UIFlex>
+          )
+      })
+  }
+
     render() {
 
-        let { content, isLoading, error } = this.state;
+        let { blogPosts, isLoading, error } = this.state;
+
+        const primaryBlogPostCard = blogPosts.map((post, index) => {
+            if (index == 0) {
+                return <BlogPostCardPrimary post={post} key={index} />
+            }
+        });
+
+        const secondaryBlogPostCards = blogPosts.map((post, index) => {
+            if (index > 0 && index < 4) {
+                return <BlogPostCardSecondary post={post} key={index} />
+            }
+        });
+
+        const defaultBlogPostCards = blogPosts.map((post, index) => {
+            if (index > 4) {
+                return (
+                    <UIFlex basis="49%" grow={0} shrink={0} key={index}>
+                        <BlogPostCard post={post}  />
+                    </UIFlex>
+                )
+            }
+        })
 
         if (error) {
-            return <p>{error.message}</p>;
+            // TODO: create error page layout
+            return (
+                <RouteTransition>
+                    <p>{error.message}</p>;
+                </RouteTransition>
+            );
         }
 
         if (isLoading) {
-            return <h1>Loading the page right now chief...</h1>;
+            // TODO: create loading page layout
+            return (
+                <RouteTransition>
+                    <h1>Loading the page right now chief...</h1>;
+                </RouteTransition>
+            );
         }
 
         return (
-            <section>
-                <UILayout className="p-top-of-page p-bottom-6">
-                {
-                    content.map( (post, index) => {
-                        switch (index) {
-                            case 0 :
-                                return <BlogPostCardPrimary post={post} key={index}/>;
-                            case 1 :
-                            case 2 :
-                            case 3 :
-                                return <BlogPostCardSecondary post={post} key={index}/>
-                            default:
-                                return <BlogPostCard post={post} key={index}/>
-                        }
-                    }
-                )}
+            <RouteTransition>
+                <Helmet title="Blog" />
+                <UILayout className="p-top-of-page p-bottom-1">
+                    <UISection className="index-2">
+                        <UIFlexRow justify="space-between" className="m-bottom-6 m-sm-bottom-12">
+                            <UIFlex basis="57%" grow={0} shrink={0}>
+                                <UIFlexRow className="full-height">
+                                    { primaryBlogPostCard }
+                                </UIFlexRow>
+                            </UIFlex>
+                            <UIFlex basis="41%" grow={0} shrink={0}>
+                                <UIFlexRow justify="space-between" direction="column" className="full-height">
+                                    { secondaryBlogPostCards }
+                                </UIFlexRow>
+                            </UIFlex>
+                        </UIFlexRow>
+                    </UISection>
                 </UILayout>
-            </section>
+                <UILayout use="secondary" className="p-top-6 p-bottom-8">
+                    <UISection className="index-2">
+                        <UIFlexRow justify="space-between" className="m-bottom-6 m-sm-bottom-12">
+                            { defaultBlogPostCards }
+                        </UIFlexRow>
+                    </UISection>
+                </UILayout>
+                
+                {   // TODO: WIP really flesh out this page and the directions that the links take you
+                    /* 
+                    <UILayout use="tertiary">
+                        <UISection classname="index-3">
+                            <UIFlexRow justify="space-between" className="m-bottom-6 m-sm-bottom-12">
+                                { this.renderCards() }
+                            </UIFlexRow>
+                        </UISection>
+                    </UILayout> 
+                    */
+                }
+            </RouteTransition>
         )
     }
 }
