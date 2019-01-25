382c696c7c4e4f7c88c8588793b9d4e99b83f07d
muigaiunaka <muigaiunaka@gmail.com>
2019-01-25 11:42:04 -0500

    ribbon cutting on Blog single post page

--- src/pages/Blog/BlogPost/BlogPost.js
+++ src/pages/Blog/BlogPost/BlogPost.js
@@ -1,71 +1,74 @@
 import React, { PureComponent } from 'react';
-import UIButton from 'UILibrary/button/UIButton';
+import RouteTransition from 'components/RouteTransition/RouteTransition';
 import UILayout from 'UILibrary/layout/UILayout';
+import UISection from 'UILibrary/layout/UISection';
 import styles from './BlogPost.scss';
-import BlogPostCardPrimary from '../BlogPostCardPrimary/BlogPostCardPrimary';
 
-
-let primaryBlogPost = (post) => {
-    return (
-        <BlogPostCardPrimary post = {post} />
-    )
-}
-
-let secondaryBlogPost = (post) => {
-    return (
-        <div key={post.title} className="blog-post-card">
-            <h4>{post.categories[0].name}</h4>
-            <h3>{post.title}</h3>
-        </div>
-    )
-}
-
-let defaultBlogPost = (post) => {
-    return (
-        <div key={post.title} className="blog-post-card">
-            <p>Default</p>
-            <h4>{post.categories[0].name}</h4>
-            <h1>{post.title}</h1>
-        </div>
-    )
-}
+import Helmet from "react-helmet";
+import {Link} from 'react-router';
+import Butter from 'buttercms';
+const butter = Butter('3b949f80849c37c52f9a9bb1a7c5d5d393e7895b');
 
 class BlogPost extends PureComponent {
     constructor(props) {
         super(props);
+        this.state = {
+            loaded: false
+        };
     }
-    renderClassNames = () => {
-        const { index, post } = this.props;
-        switch (index) {
-            case 0 :
-                return 'primary';
-            case 1 :
-            case 2 :
-            case 3 :
-                return 'secondary';
-            default : 
-                return 'default'
-        }
+    
+
+    componentDidMount() {
+        let slug = this.props.params.slug;
+        butter.post.retrieve(slug).then((resp) => {
+            console.log(resp);
+            this.setState({
+              loaded: true,
+              post: resp.data.data
+            })
+        });
+        console.log(window.location.href);
+        console.log(this.props);
+        console.log(this.props.params.slug);
     }
-    renderBlogPost = () => {
-        const { index, post } = this.props;
-        switch (index) {
-            case 0 :
-                return primaryBlogPost(post);
-            case 1 :
-            case 2 :
-            case 3 :
-                return secondaryBlogPost(post);
-            default:
-                return defaultBlogPost(post);
-        }
-    }
+
     render() {
-        return (
-            <div className={`blog-post ${this.renderClassNames()}`}>
-                { this.renderBlogPost() }
-            </div>
-        )
+        if (this.state.loaded) {
+            const post = this.state.post;
+            let timestampOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+            let timestamp = new Date(post.created).toLocaleDateString("en-US", timestampOptions)
+
+            return (
+                <RouteTransition>
+                    <Helmet>
+                        { post.seo_title && <title>{post.seo_title}</title>}
+                        { post.meta_description && <meta name="description" content={post.meta_description} /> }
+                        { post.featured_image && <meta name="og:image" content={post.featured_image} /> }
+                    </Helmet>
+                    <UILayout className="p-top-of-page p-bottom-1">
+                        <UISection className="index-2">
+                            <div className="blog-post">        
+                                <a href="/blog"></a>
+                                <Link className="text-on-dark" to="/blog">
+                                    &larr; Back to blog
+                                </Link>
+                                <h3>{post.title}</h3>
+                                <time>{timestamp}</time>
+                                <div dangerouslySetInnerHTML={{__html: post.body}} />
+                            </div>
+                        </UISection>
+                    </UILayout>
+                </RouteTransition>
+            )
+        } else {
+            return (
+                <RouteTransition>
+                    <UILayout className="p-top-of-page p-bottom-1">
+                        <div>Loadding ....</div>
+                    </UILayout>
+                </RouteTransition>
+            )
+        }
     }
 }
 
