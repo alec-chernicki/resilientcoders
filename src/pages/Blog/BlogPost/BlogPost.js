9eb63d418a32e58f02fa5aeb708e278757c4a0ee
muigaiunaka <muigaiunaka@gmail.com>
2019-03-01 15:54:56 -0500

    initial post styling inspo from bnbair

--- src/pages/Blog/BlogPost/BlogPost.js
+++ src/pages/Blog/BlogPost/BlogPost.js
@@ -1,6 +1,11 @@
 import React, { PureComponent } from 'react';
 import RouteTransition from 'components/RouteTransition/RouteTransition';
+import UIImage from 'UILibrary/image/UIImage';
 import UILayout from 'UILibrary/layout/UILayout';
+import UICard from 'UILibrary/layout/UICard';
+import UIFlex from 'UILibrary/grid/UIFlex';
+import UIFlexRow from 'UILibrary/grid/UIFlexRow';
+import UILoading from 'UILibrary/loading/UILoading';
 import UISection from 'UILibrary/layout/UISection';
 import styles from './BlogPost.scss';
 
@@ -37,7 +42,11 @@ class BlogPost extends PureComponent {
         if (this.state.loaded) {
             const post = this.state.post;
             let timestampOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-            let timestamp = new Date(post.created).toLocaleDateString("en-US", timestampOptions)
+            let timestamp = new Date(post.created).toLocaleDateString("en-US", timestampOptions);
+            let featured_image = post.featured_image ? post.featured_image : '#';
+            let authorFullName = `${post.author.first_name} ${post.author.last_name}`;
+            let authorImage = post.author.profile_image;
+            let postCategory = post.categories.length > 0 ? post.categories[0].name : "Post";
 
             return (
                 <RouteTransition>
@@ -46,8 +55,41 @@ class BlogPost extends PureComponent {
                         { post.meta_description && <meta name="description" content={post.meta_description} /> }
                         { post.featured_image && <meta name="og:image" content={post.featured_image} /> }
                     </Helmet>
-                    <UILayout className="p-top-of-page p-bottom-1">
+                    <UILayout className="p-top-of-page p-bottom-4">
+                        <UIImage
+                            className={styles['header-image']}
+                            type="header"
+                            src={featured_image}
+                            alt="resilient blog post header"
+                        />
                         <UISection className="index-2">
+                            <UIFlexRow>
+                                <UIFlex basis="75%">
+                                    <UICard className="p-all-6 pull-up-6 with-shadow">
+                                        <h2>{post.title}</h2>
+                                        <div className="divider" />
+                                        <UIImage 
+                                            src={authorImage}
+                                            type="inline"
+                                            className={styles['header-image']}
+                                        >
+                                            <h4>
+                                                {authorFullName} &#9473; {timestamp}
+                                            </h4>
+                                        </UIImage>
+                                    </UICard>
+                                </UIFlex>
+                                <UIFlex basis="25%" />
+                            </UIFlexRow>
+                            <UIFlexRow>
+                                <UIFlex basis="35%" />
+                                <UIFlex basis="65%">
+                                    <div dangerouslySetInnerHTML={{__html: post.body}} />
+                                </UIFlex>
+                            </UIFlexRow>
+                        </UISection>
+
+                        {/* <UISection className="index-2">
                             <div className="blog-post">        
                                 <a href="/blog"></a>
                                 <Link className="text-on-dark" to="/blog">
@@ -57,16 +99,14 @@ class BlogPost extends PureComponent {
                                 <time>{timestamp}</time>
                                 <div dangerouslySetInnerHTML={{__html: post.body}} />
                             </div>
-                        </UISection>
+                        </UISection> */}
                     </UILayout>
                 </RouteTransition>
             )
         } else {
             return (
                 <RouteTransition>
-                    <UILayout className="p-top-of-page p-bottom-1">
-                        <div>Loadding ....</div>
-                    </UILayout>
+                    <UILoading />
                 </RouteTransition>
             )
         }
