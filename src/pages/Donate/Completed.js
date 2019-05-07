ff38fdff1a3c81ebc460133f1bcd842b23c31881
muigaiunaka <muigaiunaka@gmail.com>
2019-05-07 17:33:06 -0400

    init donate completed page

---
+++ src/pages/Donate/Completed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Completed extends React.Component {
+    render() {
+        return (
+            <div>
+                Completed Page
+            </div>
+        )
+    }
+}
+
+export default Completed;
