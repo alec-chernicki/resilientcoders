b6ce7769ddb8ba7b0afaba428197dc77adf40517
muigaiunaka <muigaiunaka@gmail.com>
2019-05-07 17:32:55 -0400

    init donate cancel page

---
+++ src/pages/Donate/Cancel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Cancel extends React.Component {
+    render() {
+        return (
+            <div>
+                Cancel Page
+            </div>
+        )
+    }
+}
+
+export default Cancel;
