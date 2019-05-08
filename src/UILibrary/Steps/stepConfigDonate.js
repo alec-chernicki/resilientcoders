b99181fa923c6dcea6cb6cd911aa5434d3713fcd
muigaiunaka <muigaiunaka@gmail.com>
2019-05-08 16:36:18 -0400

    step config for donate page

--- src/UILibrary/Steps/stepConfigDonate.js
+++ src/UILibrary/Steps/stepConfigDonate.js
@@ -1,6 +1,5 @@
-import bootcampImage from './bootcampImage.jpg';
-import labImage from './labImage.png';
-
+const matchImage = `https://images.pexels.com/photos/39244/match-fire-close-burn-39244.jpeg?cs=srgb&dl=close-up-fire-flame-39244.jpg&fm=jpg`;
+const mailImage = `https://images.pexels.com/photos/211290/pexels-photo-211290.jpeg?cs=srgb&dl=blur-card-cardboard-211290.jpg&fm=jpg`;
 const laptopImage = `https://images.pexels.com/photos/1181233/pexels-photo-1181233.jpeg?cs=srgb&dl=beautiful-connection-education-1181233.jpg&fm=jpg`;
 const stockImage = `https://images.pexels.com/photos/1073765/pexels-photo-1073765.jpeg?cs=srgb&dl=data-electronics-exchange-1073765.jpg&fm=jpg`;
 
@@ -8,21 +7,17 @@ export default [
   {
     name: "Match Donation",
     title: 'Match Donation',
-    text: `Community is our open-door drop-in program. Current Bootcampers use this time as a study hall, supported
-    by staff and volunteers. Aspiring Bootcampers stop by to learn more before applying. Alumni who are in the workforce
-    use this time to improve skills relevant to their jobs. We are also joined by younger coders, who are still in
-    high school and want to explore development in a safe, supportive, positive space.`,
-    image: bootcampImage,
+    text: `Companies sometime match donations. Lorem ipsum palce malgnau talbot
+    abstropf kfnsal llaismre melai basicl oufen besslevon musslebeech valar morgohulis`,
+    image: matchImage,
     link: '#',
   },
   {
     name: "Mail Donation",
     title: 'Mail a Donation via Check',
-    text: `Community is our open-door drop-in program. Current Bootcampers use this time as a study hall, supported
-    by staff and volunteers. Aspiring Bootcampers stop by to learn more before applying. Alumni who are in the workforce
-    use this time to improve skills relevant to their jobs. We are also joined by younger coders, who are still in
-    high school and want to explore development in a safe, supportive, positive space.`,
-    image: labImage,
+    text: `Send us a donation to Boston. Lorem ipsum palce malgnau talbot
+    abstropf kfnsal llaismre melai basicl oufen besslevon musslebeech valar morgohulis`,
+    image: mailImage,
     link: '#',
   },
   {
@@ -34,8 +29,9 @@ export default [
   },
   {
     name: "Donate Stocks",
-    title: 'Consider Donating Equity',
-    text: `Stocks we accept those`,
+    title: `Consider Donating Equity`,
+    text: `Stocks we accept those. Lorem ipsum palce malgnau talbot
+    abstropf kfnsal llaismre melai basicl oufen besslevon musslebeech valar morgohulis`,
     image: stockImage,
     link: '/donate',
   },
