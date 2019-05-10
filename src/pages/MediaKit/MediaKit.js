0dd184b0102b942a602f206ac4ce9046b5f3bd90
muigaiunaka <muigaiunaka@gmail.com>
2019-05-10 16:17:55 -0400

    updated svg using svgo and did some restructuring

--- src/pages/MediaKit/MediaKit.js
+++ src/pages/MediaKit/MediaKit.js
@@ -5,26 +5,35 @@ import classNames from 'classnames';
 import RouteTransition from '../../components/RouteTransition/RouteTransition';
 
 import styles from './MediaKit.css';
+import UIButton from 'UILibrary/button/UIButton';
 import UILayout from 'UILibrary/layout/UILayout';
 import UISection from 'UILibrary/layout/UISection';
 import UIFlexRow from 'UILibrary/grid/UIFlexRow';
 import UIFlex from 'UILibrary/grid/UIFlex';
-import logoDark from './brand/svg/resilient_coders_logo_dark.svg';
-import logoPrimary from './brand/svg/resilient_coders_logo_primary.svg';
-import logo from './brand/svg/resilient_coders_logo_ko.svg';
-// logotype
-import logotypeDark from './brand/svg/resilient_coders_logo_logotype_dark.svg';
-import logotypePrimary from './brand/svg/resilient_coders_logo_logotype_primary.svg';
-import logotype from './brand/svg/resilient_coders_logo_logotype_ko.svg';
+
+import logosConfig from './logosConfig';
+
 
 class MediaKit extends React.PureComponent {
+    renderLogos() {
+        return logosConfig.map((logo, key) => {
+            return (
+                <div styleName="media-logo" key={key}>
+                    <img src={logo.image} alt={logo.alt} />
+                    <UIButton download href={logo.image} type="link" className="text-center">
+                        Download Logo
+                    </UIButton>
+                </div>
+            );
+        });
+    }
     render() {
         return (
             <RouteTransition>
                 <Helmet title="Media Kit" />
                 <UILayout className="p-top-of-page p-bottom-6" use="secondary">
                     <UISection className="p-top-6">
-                        <h1 className="text-darkest text-center">Resilient Coders Brand Guidelines</h1>
+                        <h1 className="text-darkest">Resilient Coders Brand Guidelines</h1>
                         <UIFlexRow>
                             <p>This is the hub for all brand guidelines, usages, and assets. We built this page to make it as easy as possible for you to deliver on-brand designs while respecting our brand and legal/licensing restrictions.</p>
                         </UIFlexRow>
@@ -32,51 +41,31 @@ class MediaKit extends React.PureComponent {
                 </UILayout>
                 <UILayout className="p-top-6" use="primary">
                     <UISection>
-                        <h2 className="text-white">The Logo</h2>
-                        <button download>Download Logo</button>
-                        <UIFlexRow className="m-y-1">
-                            <UIFlex grow={1}>
-                                <img src={logoDark} alt="Resilient Coders logo dark" />
-                            </UIFlex>
-                            <UIFlex grow={1}>
-                                <img src={logoPrimary} alt="Resilient Coders logo primary" />
-                            </UIFlex>
-                            <UIFlex grow={1}>
-                                <img src={logo} alt="Resilient Coders logo" />
-                            </UIFlex>
+                        <UIFlexRow className="p-top-6">
+                            <UISection>
+                            <h2>Our Logo: The Skull</h2>
+                            <p>Delmarism about the skull and it's meaning and greatness</p>
+                            </UISection>
+                            {this.renderLogos()}
                         </UIFlexRow>
-                        <UIFlexRow className="m-y-1">
-                            <UIFlex grow={1}>
-                                <img src={logotypeDark} alt="Resilient Coders logo dark" />
-                            </UIFlex>
-                            <UIFlex grow={1}>
-                                <img src={logotypePrimary} alt="Resilient Coders logo primary" />
-                            </UIFlex>
-                            <UIFlex grow={1}>
-                                <img src={logotype} alt="Resilient Coders logo" />
-                            </UIFlex>
-                        </UIFlexRow>
-                        <h2 className="text-white">Color Palette</h2>
-                        <UIFlexRow>
+                     
+                        <UIFlexRow className="p-top-6">
+                            <h2>Our Colors: </h2>
+                            <p>Our colors are bold, enthusiastic and full of energy. To ensure a unified brand, it is important to use these colors and these colors only.</p>
                             <div styleName="media-color-primary" className="m-r-1 m-y-1">#ED1B24</div>
                             <div styleName="media-color-secondary" className="m-r-1 m-y-1">#21262D</div>
                         </UIFlexRow>
-                        <UIFlexRow>
+                        <UIFlexRow justify="space-between">
                             <div styleName="media-color-gray-0" className="m-r-1 m-y-1">#EAEEF5</div>
                             <div styleName="media-color-gray-1" className="m-r-1 m-y-1">#EDEEF0</div>
                             <div styleName="media-color-gray-2" className="m-r-1 m-y-1">#DDDDDD</div>
-                        </UIFlexRow>
-                        <UIFlexRow>
                             <div styleName="media-color-gray-3" className="m-r-1 m-y-1">#CCCCCC</div>
                             <div styleName="media-color-gray-4" className="m-r-1 m-y-1">#4F5A60</div>
                             <div styleName="media-color-gray-5" className="m-r-1 m-y-1">#21262D</div>
                         </UIFlexRow>
-                        <UIFlexRow className="p-bottom-6">
-                            <p>Our colors are bold, enthusiastic and full of energy. To ensure a unified brand, it is important to use these colors and these colors only.</p>
-                        </UIFlexRow>
-                        <h2 className="text-white">Fonts</h2>
-                        <h6 className="text-white">League Gothic</h6>
-                        <p className="text-white">Roboto Slab</p>
+                        <h2>Fonts</h2>
+                        <h6>League Gothic</h6>
+                        <p>Roboto Slab</p>
                     </UISection>
                 </UILayout>
             </RouteTransition>
---
+++ src/pages/MediaKit/logosConfig.js
@@ -0,0 +1,50 @@
+import logoDark from './brand/svg/resilient_coders_logo_dark.svg';
+import logoPrimary from './brand/svg/resilient_coders_logo_primary.svg';
+import logo from './brand/svg/resilient_coders_logo_ko.svg';
+// logotype
+import logotypeDark from './brand/svg/resilient_coders_logo_logotype_dark.svg';
+import logotypePrimary from './brand/svg/resilient_coders_logo_logotype_primary.svg';
+import logotype from './brand/svg/resilient_coders_logo_logotype_ko.svg';
+// stacked
+import logoStackedDark from './brand/svg/resilient_coders_logo_stacked_dark.svg';
+import logoStackedPrimary from './brand/svg/resilient_coders_logo_stacked_primary.svg';
+import logoStacked from './brand/svg/resilient_coders_logo_stacked_ko.svg';
+
+export default [
+    {
+        image: logoPrimary,
+        alt: `Resilient Coders primary logo`
+    },
+    {
+        image: logo,
+        alt: `Resilient Coders logo`
+    },
+    {
+        image: logoDark,
+        alt: `Resilient Coders dark logo`
+    },
+    {
+        image: logotypePrimary,
+        alt: `Resilient Coders primary logo with name`
+    },
+    {
+        image: logotype,
+        alt: `Resilient Coders logo with name`
+    },
+    {
+        image: logotypeDark,
+        alt: `Resilient Coders dark logo with name`
+    },
+    {
+        image: logoStackedPrimary,
+        alt: `Resilient Coders primary logo stacked`
+    },
+    {
+        image: logoStacked,
+        alt: `Resilient Coders logo stacked`
+    },
+    {
+        image: logoStackedDark,
+        alt: `Resilient Coders dark logo stacked`
+    },
+]
