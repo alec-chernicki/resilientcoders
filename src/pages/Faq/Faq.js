c0c360b2a91fae594d99f605bd35122d594814c3
muigaiunaka <muigaiunaka@gmail.com>
2019-05-15 23:58:26 -0400

    updated work on FAQ

--- src/pages/Faq/Faq.js
+++ src/pages/Faq/Faq.js
@@ -6,95 +6,43 @@ import Helmet from 'react-helmet';
 
 import UILayout from 'UILibrary/layout/UILayout';
 import UISection from 'UILibrary/layout/UISection';
+import faqConfig from './faqConfig';
 
 class Faq extends React.PureComponent {
   componentDidMount() {
 
   }
+  renderFaqs() {
+    return faqConfig.map( (faq, i) => {
+        return (
+            <details>
+                <summary>{faq.question}</summary>
+                <p>{faq.answer}</p>
+            </details>
+        );
+    })
+  }
   
   render() {
-    const qa = [
-        {
-            question: "What age group is this program for?",
-            answer: "Ages 18 to 30 can participate in the bootcamp. Older come to community"
-        },
-        {
-            question: "How much does the bootcamp cost?",
-            answer: "The program is free. "
-        },
-        {
-            question: "What is people of color?",
-            answer: "The population we serve"
-        },
-        {
-            question: "I have a college degree in another field, can I do the program?",
-            answer: "Wow"
-        },
-        {
-            question: "I already have a computer science degree, can I do the program?",
-            answer: "Wow"
-        },
-        {
-            question: "Is this program for career changers?",
-            answer: "Wow"
-        },
-        {
-            question: "Is this only for residents of Boston?",
-            answer: "Wow"
-        },
-        {
-            question: "Do I have to bring my own laptop?",
-            answer: "Wow"
-        },
-        {
-            question: "Do you help me get a job?",
-            answer: "Wow"
-        },
-        {
-            question: "How are you getting people jobs?",
-            answer: "Wow"
-        },
-        {
-            question: "Where are people getting jobs?",
-            answer: "Wow"
-        },
-        {
-            question: "How long does it take for somebody to get a job?",
-            answer: "On average"
-        },
-        {
-            question: "Can I work while also doing this program?",
-            answer: "On average"
-        },
-        {
-            question: "How intense is the program?",
-            answer: "On average"
-        },
-        {
-            question: "I am still in school, can I do the program in the summer and then go back to school?",
-            answer: "On average"
-        },
-        {
-            question: "My son is graduating high school this year, can he do the program?",
-            answer: "On average"
-        },
-    ]
     return (
       <RouteTransition>
         <Helmet title="FAQ" />
-        <UILayout className="p-top-of-page p-bottom-6">
+        <h2>CREATE SUB HEADER NAV MENU COMPONENT</h2>
+        Inspiration:
+        https://dribbble.com/shots/5727844-Daily-UI-92-FAQ/attachments
+        https://dribbble.com/shots/6186067-FAQ-Daily-UI
+        https://dribbble.com/shots/5790901-Benefits/attachments
+        https://cdn.dribbble.com/users/6580/screenshots/707693/faq_s.png
+        <UILayout use="secondary" className="p-top-of-page">
+            <UISection className="p-top-6">
+                <h2>We're here to help!</h2>
+                <input type="" placeholder="Find an answer"/>
+            </UISection>
+        </UILayout>
+        <UILayout className="p-top-6 p-bottom-6">
           <UISection>
               <h2>Frequently Asked Questions</h2>
-              {
-                  qa.map( (qa, i) => {
-                      return (
-                          <div>
-                              <strong>{qa.question}</strong>
-                              <p>{qa.answer}</p>
-                          </div>
-                      );
-                  })
-                }
+              {this.renderFaqs()}
           </UISection>
         </UILayout>
       </RouteTransition>
---
+++ src/pages/Faq/faqConfig.js
@@ -0,0 +1,66 @@
+export default [
+    {
+        question: "What age group is this program for?",
+        answer: "Ages 18 to 30 can participate in the bootcamp. Older come to community"
+    },
+    {
+        question: "How much does the bootcamp cost?",
+        answer: "The program is free. "
+    },
+    {
+        question: "What is people of color?",
+        answer: "The population we serve"
+    },
+    {
+        question: "I have a college degree in another field, can I do the program?",
+        answer: "Wow"
+    },
+    {
+        question: "I already have a computer science degree, can I do the program?",
+        answer: "Wow"
+    },
+    {
+        question: "Is this program for career changers?",
+        answer: "Wow"
+    },
+    {
+        question: "Is this only for residents of Boston?",
+        answer: "Wow"
+    },
+    {
+        question: "Do I have to bring my own laptop?",
+        answer: "Wow"
+    },
+    {
+        question: "Do you help me get a job?",
+        answer: "Wow"
+    },
+    {
+        question: "How are you getting people jobs?",
+        answer: "Wow"
+    },
+    {
+        question: "Where are people getting jobs?",
+        answer: "Wow"
+    },
+    {
+        question: "How long does it take for somebody to get a job?",
+        answer: "On average"
+    },
+    {
+        question: "Can I work while also doing this program?",
+        answer: "On average"
+    },
+    {
+        question: "How intense is the program?",
+        answer: "On average"
+    },
+    {
+        question: "I am still in school, can I do the program in the summer and then go back to school?",
+        answer: "On average"
+    },
+    {
+        question: "My son is graduating high school this year, can he do the program?",
+        answer: "On average"
+    },
+]
